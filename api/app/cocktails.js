6bffc0d2ed045adeeffca115ab9d20d1e96199f4
SpeedrunDi <amanmaksat0@gmail.com>
2022-10-27 22:04:26 +0600

    CA-3 updated cocktails routes.

--- api/app/cocktails.js
+++ api/app/cocktails.js
@@ -5,6 +5,9 @@ const {nanoid} = require('nanoid');
 
 const Cocktail = require("../models/Cocktail");
 const config = require('../config');
+const auth = require("../middleware/auth");
+const User = require("../models/User");
+const permit = require("../middleware/permit");
 
 const router = express.Router();
 
@@ -21,10 +24,36 @@ const upload = multer({storage});
 
 router.get('/', async (req, res) => {
   const query = {};
+  query.published = true;
 
-  query.published = {$eq: true};
   try {
-    const cocktails = await Cocktail.find(query);
+    const token = req.get('Authorization');
+    const user = await User.findOne({token});
+
+    if (user && user.role === 'admin') {
+      delete query.published;
+    }
+    let cocktails;
+
+    if (!user) {
+      cocktails = await Cocktail.find(query);
+    } else {
+      cocktails = await Cocktail
+        .find({$or: [query, {user: user._id}]})
+        .sort({published: 1});
+    }
+
+    res.send(cocktails);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.get('/my_cocktails', auth, async (req, res) => {
+  try {
+    const cocktails = await Cocktail
+      .find({user: req.user._id})
+      .sort({published: 1});
 
     res.send(cocktails);
   } catch (e) {
@@ -40,13 +69,24 @@ router.get('/:id', async (req, res) => {
   }
 
   const query = {};
-
   query.published = {$eq: true};
-
   query._id = id
 
   try {
-    const cocktail = await Cocktail.findOne(query);
+    const token = req.get('Authorization');
+    const user = await User.findOne({token});
+
+    if (user && user.role === 'admin') {
+      delete query.published;
+    }
+
+    let cocktail;
+
+    if (!user) {
+      cocktail = await Cocktail.findOne(query);
+    } else {
+      cocktail = await Cocktail.findOne({$or: [{$and: [{user: user._id}, {_id: id}]}, query]});
+    }
 
     if (!cocktail) {
       return res.status(404).send({message: 'Cocktail not found!'});
@@ -58,13 +98,14 @@ router.get('/:id', async (req, res) => {
   }
 });
 
-router.post('/new_cocktail', upload.single('image'), async (req, res) => {
+router.post('/new_cocktail', auth, upload.single('image'), async (req, res) => {
   const {title, recipe, ingredients} = req.body;
 
   const cocktailData = {
     title,
     recipe,
-    ingredients
+    ingredients,
+    user: req.user._id
   };
 
   if (req.file) cocktailData.image = 'uploads/' + req.file.filename;
@@ -73,6 +114,54 @@ router.post('/new_cocktail', upload.single('image'), async (req, res) => {
     const cocktail = new Cocktail(cocktailData);
 
     await cocktail.save();
+
+    res.send(cocktail);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+router.patch('/:id', auth, permit('admin'), async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    if (!id) {
+      return res.status(400).send({error: 'ID not valid'});
+    }
+
+    const cocktail = await Cocktail.findOneAndUpdate({
+      _id: id
+    }, {
+      published: true
+    }, {
+      returnDocument: 'after',
+    });
+
+    if (!cocktail) {
+      return res.status(404).send({message: "Cocktail not found!"});
+    }
+
+    res.send(cocktail);
+  } catch (e) {
+    res.sendStatus(500);
+  }
+});
+
+router.delete('/:id', auth, permit('admin'), async (req, res) => {
+  const id = req.params.id;
+
+  if (!id) {
+    return res.status(400).send({message: 'ID not valid'});
+  }
+
+  try {
+    const cocktail = await Cocktail.findOneAndDelete({
+      _id: id
+    });
+
+    if (!cocktail) {
+      return res.status(404).send({message: "Cocktail not found!"});
+    }
 
     res.send(cocktail);
   } catch (e) {
